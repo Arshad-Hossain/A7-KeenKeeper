345065912fd16943acb9355993e8cd941508e18e
arshadhossainoh <arshadhossain.oh@gmail.com>
2026-04-14 01:06:38 -0400

    worked on routing part & can navigate across pages

--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -3,26 +3,32 @@ import React from "react";
 import { IoHomeOutline } from "react-icons/io5";
 import { PiChartLine } from "react-icons/pi";
 import { RiTimeLine } from "react-icons/ri";
+import { NavLink } from "react-router";
 
 const Navbar = () => {
   return (
-    <div className="flex justify-between mx-15 p-4 bg-[#ffffff]">
+    <nav className="flex justify-between mx-15 p-4 bg-[#ffffff]">
       <h1 className="font-bold">
         Keen<span className="text-[#244d3f]">Keeper</span>
       </h1>
 
       <ul className="flex justify-between gap-5">
-        <li className="flex items-center bg-[#244d3f] text-white p-1">
+        <NavLink
+          to={"/"}
+          className="flex items-center bg-[#244d3f] text-white p-1"
+        >
           <IoHomeOutline></IoHomeOutline>Home
-        </li>
-        <li className="flex items-center">
+        </NavLink>
+
+        <NavLink to={"/timeline"} className="flex items-center">
           <RiTimeLine></RiTimeLine> Timeline
-        </li>
-        <li className="flex items-center">
+        </NavLink>
+
+        <NavLink to={"/stats"} className="flex items-center">
           <PiChartLine></PiChartLine> Stats
-        </li>
+        </NavLink>
       </ul>
-    </div>
+    </nav>
   );
 };
 
